7ad8c726d295a1bc2b12dca0b33cf7b2478183de
Mikhail Baldaev <baldaevm@gmail.com>
2023-01-25 16:12:21 +0300

    Initial

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
+from books.models import Book
 
+
+def index(request):
+
+    return redirect('books')
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books = Book.objects.all()
+    context = {'books_list': books}
+
+    return render(request, template, context)
+
+
+def book_date(request, pub_date):
+    book = get_object_or_404(Book, pub_date=pub_date)
+
+    try:
+        book.get_previous_by_pub_date()
+        previous_book = book.get_previous_by_pub_date()
+    except:
+        previous_book = None
+
+    try:
+        book.get_next_by_pub_date()
+        next_book = book.get_next_by_pub_date()
+    except:
+        next_book = None
+
+    template = 'books/book_date.html'
+    context = {
+        'book': book,
+        'previous': previous_book,
+        'next': next_book,
+    }
+
     return render(request, template, context)
